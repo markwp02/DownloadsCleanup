8411306836e117330ff17461db97dae817892d04
Mark <markwp02@gmail.com>
2023-10-06 16:08:29 +1100

    Simple File Route
    - Simple Route created to move files from one directory to another

---
+++ src/main/java/com/markp/downloadscleanup/DownloadsCleanupApplication.java
@@ -0,0 +1,13 @@
+package com.markp.downloadscleanup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DownloadsCleanupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DownloadsCleanupApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/markp/downloadscleanup/DownloadsCleanupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.markp.downloadscleanup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DownloadsCleanupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
