ba6634a7539f85ecda73f47943c6c8bf3091dd5f
Mark <markwp02@gmail.com>
2023-10-11 17:21:13 +1100

    Add image, compressed, app types and refactor
    Features
    - Add image destination type
    - Add compressed destination type
    - Add app destination type
    
    Refactor
    - Use base.destination.folder property to set base destination
    - Use sub folder properties for each file extension
    - Changing base destination for all file types can be accomplished with one property change

--- src/main/java/com/markp/downloadscleanup/routes/FileRouter.java
+++ src/main/java/com/markp/downloadscleanup/routes/FileRouter.java
@@ -14,32 +14,56 @@ public class FileRouter extends RouteBuilder {
     @Value("${source.folder}")
     private String sourceFolder;
 
-    @Value("${code.destination.folder}")
-    private String codeDestinationBaseFolder;
+    @Value("${base.destination.folder}")
+    private String baseDestinationFolder;
+
+    @Value("${code.sub.folder}")
+    private String codeSubFolder;
 
     @Value("${code.suffix}")
     private String codeSuffix;
 
-    @Value("${pdf.destination.folder}")
-    private String pdfDestinationBaseFolder;
+    @Value("${pdf.sub.folder}")
+    private String pdfSubFolder;
 
     @Value("${pdf.suffix}")
     private String pdfSuffix;
 
-    @Value("${text.destination.folder}")
-    private String textDestinationBaseFolder;
+    @Value("${text.sub.folder}")
+    private String textSubFolder;
 
     @Value("${text.suffix}")
     private String textSuffix;
 
+    @Value("${image.sub.folder}")
+    private String imageSubFolder;
+
+    @Value("${image.suffix}")
+    private String imageSuffix;
+
+    @Value("${compressed.sub.folder}")
+    private String compressedSubFolder;
+
+    @Value("${compressed.suffix}")
+    private String compressedSuffix;
+
+    @Value("${app.sub.folder}")
+    private String appSubFolder;
+
+    @Value("${app.suffix}")
+    private String appSuffix;
+
     @Override
     public void configure() throws Exception {
         String archiveFolder = FileUtility.getArchiveFolder();
-        String codeDestinationFolder = FileUtility.getDestinationFolder(codeDestinationBaseFolder, archiveFolder);
-        String pdfDestinationFolder = FileUtility.getDestinationFolder(pdfDestinationBaseFolder, archiveFolder);
-        String textDestinationFolder = FileUtility.getDestinationFolder(textDestinationBaseFolder, archiveFolder);
+        String codeDestinationFolder = FileUtility.getDestinationFolder(baseDestinationFolder,codeSubFolder ,archiveFolder);
+        String pdfDestinationFolder = FileUtility.getDestinationFolder(baseDestinationFolder,pdfSubFolder ,archiveFolder);
+        String textDestinationFolder = FileUtility.getDestinationFolder(baseDestinationFolder,textSubFolder ,archiveFolder);
+        String imageDestinationFolder = FileUtility.getDestinationFolder(baseDestinationFolder,imageSubFolder ,archiveFolder);
+        String compressedDestinationFolder = FileUtility.getDestinationFolder(baseDestinationFolder,compressedSubFolder ,archiveFolder);
+        String appDestinationFolder = FileUtility.getDestinationFolder(baseDestinationFolder,appSubFolder ,archiveFolder);
 
-        String sourceSuffix = FileUtility.combineRegex(new ArrayList<>(Arrays.asList(codeSuffix, pdfSuffix, textSuffix)));
+        String sourceSuffix = FileUtility.combineRegex(new ArrayList<>(Arrays.asList(codeSuffix, pdfSuffix, textSuffix, imageSuffix, compressedSuffix, appSuffix)));
 
         from("file://" + sourceFolder + "?include=" + sourceSuffix)
                 .id("FILE_ROUTE")
@@ -49,14 +73,26 @@ public class FileRouter extends RouteBuilder {
                     .setHeader("status", simple("CODE"))
                     .log("Route to code: " + codeDestinationFolder)
                     .to("file://" + codeDestinationFolder)
-                .when((exchange) -> exchange.getIn().getHeader("CamelFileName").toString().endsWith("pdf"))
+                .when((exchange) -> exchange.getIn().getHeader("CamelFileName").toString().matches(pdfSuffix))
                     .setHeader("status", simple("PDF"))
                     .log("Route to pdf: " + pdfDestinationFolder)
                     .to("file://" + pdfDestinationFolder)
-                .when((exchange) -> exchange.getIn().getHeader("CamelFileName").toString().endsWith("txt"))
+                .when((exchange) -> exchange.getIn().getHeader("CamelFileName").toString().matches(textSuffix))
                     .setHeader("status", simple("TEXT"))
                     .log("Route to text: " + textDestinationFolder)
                     .to("file://" + textDestinationFolder)
+                .when((exchange) -> exchange.getIn().getHeader("CamelFileName").toString().matches(imageSuffix))
+                    .setHeader("status", simple("IMAGE"))
+                    .log("Route to image: " + imageDestinationFolder)
+                    .to("file://" + imageDestinationFolder)
+                .when((exchange) -> exchange.getIn().getHeader("CamelFileName").toString().matches(compressedSuffix))
+                    .setHeader("status", simple("COMPRESSED"))
+                    .log("Route to compressed: " + compressedDestinationFolder)
+                    .to("file://" + compressedDestinationFolder)
+                .when((exchange) -> exchange.getIn().getHeader("CamelFileName").toString().matches(appSuffix))
+                    .setHeader("status", simple("APP"))
+                    .log("Route to app: " + appDestinationFolder)
+                    .to("file://" + appDestinationFolder)
                 .otherwise()
                     .setHeader("status", simple("FAILURE"))
                     .log("Invalid file type for file ${header.CamelFileName}")
--- src/main/java/com/markp/downloadscleanup/utilities/FileUtility.java
+++ src/main/java/com/markp/downloadscleanup/utilities/FileUtility.java
@@ -21,17 +21,19 @@ public class FileUtility {
     }
 
     /**
-     * Combines the supplied base folder and archive folder
+     * Combines the supplied base folder, sub and archive folders. Sub and archive folders optionally
+     * can have '/' chars, as it will work either way.
      * @param baseFolder
+     * @parm subFolderFolder
      * @param archiveFolder
      * @return
      */
-    public static String getDestinationFolder(String baseFolder, String archiveFolder) {
-        return baseFolder + archiveFolder;
+    public static String getDestinationFolder(String baseFolder, String subFolder, String archiveFolder) {
+        return baseFolder + "/" + subFolder + "/" + archiveFolder;
     }
 
     /**
-     * Method that will concat all regexes in the supplied list seperated with the '|' char
+     * Method that will concat all regexes in the supplied list separated with the '|' char
      *
      * @param regexList ArrayList of valid regex in the format (.*.xxx$) where "xxx" can be
      *                  replaced with the file suffix
